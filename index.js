d38f455ec26e1b646f38e7a47a8f8eeb2a23305b
Yo-Wayne <yowayne.chen@gmail.com>
2025-07-15 23:42:56 +0800

    Updates

--- index.js
+++ index.js
@@ -144,7 +144,12 @@ var vite_config_default = defineConfig({
   root: path.resolve(import.meta.dirname, "client"),
   build: {
     outDir: path.resolve(import.meta.dirname, "dist"),
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(import.meta.dirname, "client/index.html")
+      }
+    }
   },
   server: {
     fs: {
